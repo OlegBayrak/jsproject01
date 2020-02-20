2d4bbf9fb293d9d27cb11bf31cc8df6fe6c7330e
Oleg <example@gmail.com>
2020-02-20 10:39:25 +0200

    first commit

---
+++ scipt.js
@@ -0,0 +1,28 @@
+let person = {
+    name : "John",
+    age: 25,
+    isMarried: false
+};
+
+//console.log(person["name"]);
+//confirm("Are you here?");
+//let answer = confirm("Are you here?")
+//console.log(answer);
+//comments to be saved - it's a good practice
+//let arr=["1.png"];
+//let answer = prompt("Есть ли 18 лет?", "Да, ёпт, канешн");
+//console.log(answer);
+//console.log(typeof(null));
+
+//let incr=10,
+//    decr=10;
+
+
+//console.log(++incr,--decr);
+//console.log(incr++, decr--);
+//console.log(5%2); // остаток от деления
+
+let IsChecked = true;
+    IsClose = false;
+
+console.log(IsChecked || IsClose);
